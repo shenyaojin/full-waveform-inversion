91935f34c181a2f004461997f82b0a0e7bd9f933
Shenyao Jin <106902466+shenyaojin@users.noreply.github.com>
2025-06-26 10:58:09 -0600

    Implement example part 1.

--- src/core.py
+++ src/core.py
@@ -1,3 +1,12 @@
 # The core function of FWI.
+# Shenyao Jin, shenyaojin@mines.edu
+
+#%% Load libraries
 import numpy as np
-import matplotlib.pyplot as plt
+import src.io as io
+
+#%% Load data
+filepath = "data/MODEL_P-WAVE_VELOCITY_1.25m.segy.tar.gz"
+# True velocity model
+p_vel_true = io.convert_targz_segy_to_numpy(filepath)
+print("True velocity model loaded. Shape:", p_vel_true.shape)
